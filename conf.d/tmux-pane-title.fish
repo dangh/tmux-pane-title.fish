986db5893edf5eda457d96547f8cf5800d1f83ab
Dang <huynhminhdang@gmail.com>
2023-06-07 06:02:10 +0800

    Update tmux-pane-title.fish

--- conf.d/tmux-pane-title.fish
+++ conf.d/tmux-pane-title.fish
@@ -1,9 +1,9 @@
 if status is-interactive
-  function tmux_pane_title_reset -e fish_postexec
-    tmux set-option -p @pane_title ""
-  end
-  
   function tmux_pane_title_cmd -e fish_preexec
     tmux set-option -p @pane_title $argv
+  end
+
+  function tmux_pane_title_reset -e fish_postexec -e fish_posterror -e fish_cancel
+    tmux set-option -p @pane_title ""
   end
 end
