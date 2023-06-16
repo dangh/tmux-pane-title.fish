355d852c5b0eb5b0f6db9298e1731f3e1c94e5ce
Dang <huynhminhdang@gmail.com>
2023-06-16 21:34:59 +0800

    only run when inside tmux

--- conf.d/tmux-pane-title.fish
+++ conf.d/tmux-pane-title.fish
@@ -1,9 +1,11 @@
 if status is-interactive
   function tmux_pane_title_cmd -e fish_preexec
+    test -n "$TMUX" or return
     tmux set-option -t $TMUX_PANE -p @pane_title $argv
   end
 
   function tmux_pane_title_reset -e fish_postexec -e fish_posterror -e fish_cancel
+    test -n "$TMUX" or return
     tmux set-option -t $TMUX_PANE -p @pane_title ""
   end
 end
